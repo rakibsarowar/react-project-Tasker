ceaf60d8281803f460148c15d7ebbc80a8326d14
rakibsarowar <rakibsarowar@gmail.com>
2024-01-21 22:23:00 +0600

    set up project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+
+const App = () => {
+  return (
+    <div>
+      <Header></Header>
+      <HeroSection></HeroSection>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default App;
