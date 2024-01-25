14f3272ea856f9a25d1c9fbf01ada104b55fca06
rakibsarowar <rakibsarowar@gmail.com>
2024-01-25 08:03:08 +0600

    added the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import HeroSection from "./HeroSection";
+import TaskBoard from "./task/TaskBoard";
 
 const App = () => {
   return (
     <div>
       <Header></Header>
+      <div className="flex flex-col justify-center items-center">
       <HeroSection></HeroSection>
+      <TaskBoard></TaskBoard>
+      </div>
       <Footer></Footer>
     </div>
   );
